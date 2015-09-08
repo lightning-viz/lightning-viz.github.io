6daa5640344e6e3f9321d16365686ea26d649e12
Matthew Conlen <mc@mathisonian.com>
2015-09-08 00:57:28 -0400

    add custom walkthru video

--- src/js/pages/editor.js
+++ src/js/pages/editor.js
@@ -9,10 +9,6 @@ request
     .set('Accept', 'application/json')
     .end(function(err, res) {
         var viz = res.body;
-
-        console.log('sample options');
-        console.log(viz.sampleOptions);
         var Editor = require('../components/editor');
         React.render(<Editor datasets={viz.sampleData} images={viz.sampleImages} initialOptions={viz.sampleOptions} codeExamples={viz.codeExamples} moduleName={viz.moduleName} /> , document.getElementById('editor-component'));
     });
-
